a42f933bcf08ccc5a93ef9f51fd6332576a81827
wadud.hossain <wadud.hossain@efficientsoftwaresolutions.com>
2026-05-14 12:39:47 +0600

    fixing

--- src/routes/BlogPostRoute.js
+++
@@ -1,8 +0,0 @@
-import { createBlogPost } from "../controllers/BlogPostController.js";
-import express from "express";
-const router = express.Router();
-
-// Route to create a new blog post
-router.post("/", createBlogPost);
-
-export default router;
